e39fa7ced860c170b410f1684384359ec938437f
19alex76 <90281725+19alex76@users.noreply.github.com>
2021-10-11 01:35:18 +0300

    add new file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log('hello')
+var buttonElement = document.querySelector('button')
+buttonElement.addEventListener('click',function(){
+    buttonElement.classList.add('button-red')
+})
+
+
+
+
+
+
+//console.log(logoElement)
+
+
+
